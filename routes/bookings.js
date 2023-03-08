bcbbdc622b2f50c4fef88d9b2bf9d59a99e4f457
Boubaker <boubaker.kakkas@outlook.fr>
2023-03-08 14:48:42 +0100

    test

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,22 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
-const Booking = require('../models/bookings');
-const Cart = require('../models/carts');
+const Bookings = require('../models/bookings');
+const Carts = require('../models/carts');
 
 
 // Obtention du trajet sélectionné pour la réservation
-router.get('/', (req, res) => {
-  const { ObjectId } = req.query;
-  Booking.findOne({ ObjectId })
-    .then(booking => {
-      res.json({ booking });
-    })
-    .catch(err => {
-      res.json({ error: err.message });
-    });
-});
 
+router.post('/add', function (req,res){
+  const { ObjectId } = req.query;
+
+Carts.findById(ObjectId)
+.then(data => {
+  if (data) {
+      Bookings.findOne({ departure: data.departure, arrival: data.arrival, price: data.price })
+      .then(searchedOne => {
+          console.log(searchedOne)
+          if (searchedOne === null){
+              const newBookings = new Bookings({
+                  departure: data.departure,
+                  arrival: data.arrival,
+                  date: data.date,
+                  price: data.price,
+                })
+                newBookings.save()
+                .then(booking => {
+                  res.json({ result: true , message: 'Trip added to cart', booking});
+                })
+                .catch(error => {
+                  res.json({ result: false, error: error.message });
+                });
+          } else {
+              res.json({ result: false, message: 'Trip already saved' });
+          }
+      })
+      .catch(error => {
+        res.json({ result: false, error: error.message });
+      });
+
+  } else { 
+    res.json({ result: false, error: 'Trip not found' });
+  }
+})
+.catch(error => {
+  res.json({ result: false, error: error.message });
+})
+});
 
 
 module.exports = router;
--- routes/carts.js
+++ routes/carts.js
@@ -8,62 +8,59 @@ const Booking = require('../models/bookings');
 //route POST to create a new trip in cart when the book button is clicked on
 
 router.post('/add', function (req,res){
-    const { ObjectId } = req.query;
+  const { ObjectId } = req.query;
 
-    Trips.findById(ObjectId)
-      .then(data => {
-        if (data) {
-            Cart.findOne(data)
-            .then(searchedOne => {
-                console.log(searchedOne)
-                if (searchedOne === null){
-                    const newCart = new Cart({
-                        departure: data.departure,
-                        arrival: data.arrival,
-                        //date: data.date,
-                        price: data.price,
-                      })
-                      newCart.save()
-                      .then(cart => {
-                        res.json({ result: true , message: 'Trip added to cart', cart});
-                      })
-                      .catch(error => {
-                        res.json({ result: false, error: error.message });
-                      });
-                }
-            })
+  Trips.findById(ObjectId)
+    .then(data => {
+      if (data) {
+          Cart.findOne({ departure: data.departure, arrival: data.arrival, price: data.price })
+          .then(searchedOne => {
+              console.log(searchedOne)
+              if (searchedOne === null){
+                  const newCart = new Cart({
+                      departure: data.departure,
+                      arrival: data.arrival,
+                      date: data.date,
+                      price: data.price,
+                    })
+                    newCart.save()
+                    .then(cart => {
+                      res.json({ result: true , message: 'Trip added to cart', cart});
+                    })
+                    .catch(error => {
+                      res.json({ result: false, error: error.message });
+                    });
+              } else {
+                  res.json({ result: false, message: 'Trip already saved' });
+              }
+          })
+          .catch(error => {
+            res.json({ result: false, error: error.message });
+          });
 
-        } else { 
-          res.json({ result: false, error: 'Trip not found' });
-        }
-      })
-      .catch(error => {
-        res.json({ result: false, error: error.message });
-      });
-  });
-
-// Obtention du trajet sélectionné dans le panier
-router.get('/', (req, res) => {
-  const { _id } = req.query;
-  Cart.find({ _id })
-    .then(cart => {
-      res.json({ cart });
+      } else { 
+        res.json({ result: false, error: 'Trip not found' });
+      }
     })
-    .catch(err => {
-      res.json({ error: err.message });
+    .catch(error => {
+      res.json({ result: false, error: error.message });
     });
 });
 
-
 // Suppression du trajet sélectionné dans le panier
-router.delete('/', (req, res) => {
-  const { _id } = req.query;
-  Cart.deleteOne({ _id })
-    .then(cart => {
-      res.json({ message: 'Trip removed from cart' });
+router.delete('/remove', function (req,res){
+  const { ObjectId } = req.query;
+
+  Cart.findByIdAndDelete(ObjectId)
+    .then(data => {
+      if (data) {
+        res.json({ result: true , message: 'Trip removed from cart' });
+      } else { 
+        res.json({ result: false, error: 'Trip not found' });
+      }
     })
-    .catch(err => {
-      res.json({ error: err.message });
+    .catch(error => {
+      res.json({ result: false, error: error.message });
     });
 });
 
